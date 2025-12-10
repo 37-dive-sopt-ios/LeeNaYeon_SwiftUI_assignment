62fe2f00edaa04d13421bb2da78db224cd062863
yeonee <leenayeon@sookmyung.ac.kr>
2025-12-10 22:54:24 +0900

    init: 프로젝트 세팅

---
+++ sopt-37th-switfui-assignment/sopt-37th-switfui-assignment/sopt_37th_switfui_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  sopt_37th_switfui_assignmentApp.swift
+//  sopt-37th-switfui-assignment
+//
+//  Created by 이나연 on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct sopt_37th_switfui_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
